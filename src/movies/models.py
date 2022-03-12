8e09868d9c152ad9baacb673396a2df18b8320f9
Rostyslav <malanyukrostik@gmail.com>
2022-03-12 14:02:00 +0200

    add DetailView

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,5 +1,6 @@
 from datetime import date
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField("Category", max_length=150)
@@ -58,6 +59,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
 
     class Meta:
         verbose_name = "Film"
--- src/movies/views.py
+++ src/movies/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from movies.models import Movie
-from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
 
-class MoviesView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = "movies/movie_list.html"
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = "url"
+
+# class MoviesView(View):
+#     def get(self, request):
+#         movies = Movie.objects.all()
+#         return render(request, "movies/movie_list.html", {"movie_list": movies})
+
+# class MovieDetailView(View):
+#     def get(self, request, slug):
+#         movie = Movie.objects.get(url=slug)
+#         return render(request, "movies/movie_detail.html", {"movie": movie})
